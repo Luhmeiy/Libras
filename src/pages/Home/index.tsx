30f28c98afae0eb76ef69cc299e20db6f5b197ee
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2023-03-06 21:18:43 -0300

    Add f, g and h

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,7 +6,7 @@ const Home = () => {
 	return (
 		<div className="w-screen h-screen flex flex-col justify-center items-center">
 			<h1 className="text-4xl font-bold mb-2">
-				Conversor de Gestos para Libras
+				Conversor de Libras para Letras
 			</h1>
 			<p className="text-gray-500 text-lg mb-12">
 				Faça um gesto e o programa reconhecerá qual letra é!
--- src/util/gestures.ts
+++ src/util/gestures.ts
@@ -8,6 +8,9 @@ const LetterB = new GestureDescription("letter-b"); // B
 const LetterC = new GestureDescription("letter-c"); // C
 const LetterD = new GestureDescription("letter-d"); // D
 const LetterE = new GestureDescription("letter-e"); // E
+const LetterF = new GestureDescription("letter-f"); // F
+const LetterG = new GestureDescription("letter-g"); // G
+const LetterH = new GestureDescription("letter-h"); // H
 
 // A
 // -----------------------------------------------------------------------------
@@ -25,6 +28,8 @@ for (let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
 
 LetterB.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1);
 LetterB.addCurl(Finger.Thumb, FingerCurl.FullCurl, 0.5);
+LetterB.addDirection(Finger.Thumb, FingerDirection.HorizontalLeft, 0.9);
+LetterB.addDirection(Finger.Thumb, FingerDirection.HorizontalRight, 0.9);
 
 for (let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
 	LetterB.addCurl(finger, FingerCurl.NoCurl, 1.0);
@@ -46,12 +51,10 @@ for (let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
 // D
 // -----------------------------------------------------------------------------
 
+LetterD.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 0.9);
+
 LetterD.addCurl(Finger.Index, FingerCurl.NoCurl, 0.9);
 LetterD.addDirection(Finger.Index, FingerDirection.VerticalUp, 0.9);
-
-LetterD.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 0.9);
-LetterD.addCurl(Finger.Thumb, FingerCurl.NoCurl, 0.9);
-LetterD.addDirection(Finger.Thumb, FingerDirection.VerticalUp, 0.9);
 
 for (let finger of [Finger.Middle, Finger.Ring, Finger.Pinky]) {
 	LetterD.addCurl(finger, FingerCurl.HalfCurl, 0.9);
@@ -68,7 +71,57 @@ for (let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
 	LetterE.addCurl(finger, FingerCurl.HalfCurl, 1);
 }
 
-const knownGestures = [LetterA, LetterB, LetterC, LetterD, LetterE];
+// F
+// -----------------------------------------------------------------------------
+
+LetterF.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1);
+LetterF.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 0.5);
+LetterF.addDirection(Finger.Thumb, FingerDirection.VerticalUp, 1);
+
+LetterF.addCurl(Finger.Index, FingerCurl.HalfCurl, 0.9);
+
+for (let finger of [Finger.Middle, Finger.Ring, Finger.Pinky]) {
+	LetterF.addCurl(finger, FingerCurl.NoCurl, 1);
+	LetterF.addDirection(finger, FingerDirection.VerticalUp, 1);
+}
+
+// G
+// -----------------------------------------------------------------------------
+
+LetterG.addCurl(Finger.Thumb, FingerCurl.NoCurl, 0.9);
+
+LetterG.addCurl(Finger.Index, FingerCurl.NoCurl, 1);
+LetterG.addDirection(Finger.Index, FingerDirection.VerticalUp, 1);
+
+for (let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
+	LetterG.addCurl(finger, FingerCurl.FullCurl, 1.0);
+}
+
+// H
+// -----------------------------------------------------------------------------
+// Precisa ser atualizado
+
+for (let finger of [Finger.Thumb, Finger.Index, Finger.Middle]) {
+	LetterH.addCurl(finger, FingerCurl.NoCurl, 1.0);
+	LetterH.addDirection(finger, FingerDirection.VerticalUp, 0.9);
+	LetterH.addDirection(finger, FingerDirection.DiagonalUpLeft, 0.9);
+	LetterH.addDirection(finger, FingerDirection.DiagonalUpRight, 0.9);
+}
+
+for (let finger of [Finger.Ring, Finger.Pinky]) {
+	LetterH.addCurl(finger, FingerCurl.FullCurl, 1.0);
+}
+
+const knownGestures = [
+	LetterA,
+	LetterB,
+	LetterC,
+	LetterD,
+	LetterE,
+	LetterF,
+	LetterG,
+	LetterH,
+];
 
 const gestureStrings = {
 	"letter-a": "A",
@@ -76,6 +129,9 @@ const gestureStrings = {
 	"letter-c": "C",
 	"letter-d": "D",
 	"letter-e": "E",
+	"letter-f": "F",
+	"letter-g": "G",
+	"letter-h": "H",
 };
 
 export { knownGestures, gestureStrings };
